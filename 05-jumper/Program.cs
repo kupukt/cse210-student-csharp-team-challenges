3c349d96588beaefd5366d3a0eb5400cc5f666da
Scott Burton <burtons@byui.edu>
2021-10-06 22:27:08 -0600

    Initial Version

---
+++ 05-jumper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _05_jumper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting point for the Jumper project.");
+        }
+    }
+}
---
+++ 06-mastermind/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06_mastermind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting point for the Mastermind project.");
+        }
+    }
+}
