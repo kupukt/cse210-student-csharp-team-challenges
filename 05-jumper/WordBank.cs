6134098f3506a0fb8feadd32bb426a02e5119c07
kupuk <kupuktuifua@outlook.com>
2021-10-17 14:50:46 -0600

    finished word bank

--- 05-jumper/WordBank.cs
+++ 05-jumper/WordBank.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Collections.Generic;
 
 namespace _05_jumper
 {
     class WordBank
     {
         
+        
+        public string wordGenerator()
+        {
+            List<string> _wordbank = new List<string>();
+            Random wordsgenerator = new Random();
+           
+            _wordbank.Add("Tacos");
+            _wordbank.Add("Lacrosse");
+            _wordbank.Add("Apocolypse");
+            _wordbank.Add("Strawberry");
+
+            string _word = _wordbank[wordsgenerator.Next(1,_wordbank.Count)];
+
+            return _word;
+
+            
+        }
+
     }
 }
